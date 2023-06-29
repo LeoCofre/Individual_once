2d952d4897285474ea7f407bbe1787b16618db3b
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-28 21:30:57 -0400

    Se agregan componentes visuales

--- app/src/main/java/cl/awakelab/individualonce/MainActivity.java
+++ app/src/main/java/cl/awakelab/individualonce/MainActivity.java
@@ -3,12 +3,27 @@ package cl.awakelab.individualonce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import cl.awakelab.individualonce.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(v -> Toast.makeText(this, "Turn off flight mode", Toast.LENGTH_LONG).show());
+        binding.button2.setOnClickListener(v -> Toast.makeText(this, "Updating profile", Toast.LENGTH_LONG).show());
+        binding.button3.setOnClickListener(v -> Toast.makeText(this, "Picture save to gallery", Toast.LENGTH_LONG).show());
+        binding.button4.setOnClickListener(v -> Toast.makeText(this, "unlocked level 5", Toast.LENGTH_LONG).show());
+        binding.button5.setOnClickListener(v -> Toast.makeText(this, "Level 4 completed", Toast.LENGTH_LONG).show());
+        binding.button6.setOnClickListener(v -> Toast.makeText(this, "Music paused", Toast.LENGTH_LONG).show());
     }
+
+
+
 }
